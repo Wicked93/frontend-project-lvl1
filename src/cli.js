de3c0a6523600a5175c48beda65d06506b6fc519
Anton Zlobin <zlobinanton20@gmail.com>
2022-06-29 17:20:28 +0500

    export default

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const addName = () => {
+export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   return console.log(`Hello, ${name}!`);
 };
-
-// eslint-disable-next-line import/prefer-default-export
-export { addName };
